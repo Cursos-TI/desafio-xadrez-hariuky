17141b2476e8572c3a6d295d453809647186c625
Gleidson <152668886+hariuky@users.noreply.github.com>
2025-08-19 19:10:19 -0300

    xadrezMestre.c

--- xadrez.c
+++ xadrez.c
@@ -1,35 +1,64 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Função recursiva para simular o movimento da Torre
+
+void moverTorre(int passos_restantes) {
+   
+    if (passos_restantes <= 0) {
+        return; 
+    }
+    
+    printf("Direita\n");
+    moverTorre(passos_restantes - 1);
+}
+
+// Função recursiva com loops aninhados para o movimento do Bispo
+void moverBispo(int passos_restantes) {
+    if (passos_restantes <= 0) {
+        return;
+    }
+    
+    // Loop externo (vertical): imprime a parte vertical do movimento
+    printf("Cima,");
+    
+    // Loop interno (horizontal): imprime a parte horizontal
+    for (int i = 0; i < 1; i++) {
+        printf("Direita\n");
+    }
+
+    moverBispo(passos_restantes - 1);
+}
+
+// Função recursiva para simular o movimento da Rainha
+void moverRainha(int passos_restantes) {
+    if (passos_restantes <= 0) {
+        return;
+    }
+    
+    printf("Esquerda\n");
+    moverRainha(passos_restantes - 1);
+}
+
 int main(int argc, char *argv[]) {	
-	int var = 0;
 	// Constantes para o número de movimentos de cada peça
-	const int torre = 5;
-	const int bispo = 5;
-	const int rainha = 8;
+	const int torre_passos = 5;
+	const int bispo_passos = 5;
+	const int rainha_passos = 8;
 	const int cavalo_vertical = 2;
 	const int cavalo_horizontal = 1;
-
-	// Simulação do movimento da Torre
-	printf("Torre movendo...\n");
-	for(int i = 0; i < torre; i++){
-		printf("Direita\n");
-	}
-		
-	// Simulação do movimento do Bispo
-	printf("\nBispo movendo...\n");
-	while(var < bispo){
-		printf("Cima-direita\n");
-		var++;
-	}
 				
-	// Simulação do movimento da Rainha
+	// Simulação do movimento da Torre usando recursividade
+	printf("Torre movendo...\n");
+	moverTorre(torre_passos);
+		
+	// Simulação do movimento do Bispo usando recursividade e loops aninhados
+	printf("\nBispo movendo...\n");
+	moverBispo(bispo_passos);
+				
+	// Simulação do movimento da Rainha usando recursividade
 	printf("\nRainha movendo...\n");
-	var = 0; 
-	do{
-		printf("Esquerda\n");
-		var ++;
-	}while(var < rainha);
+	moverRainha(rainha_passos);
 	
 	// Simulação do movimento do cavalo
 
@@ -37,7 +66,9 @@ int main(int argc, char *argv[]) {
 	for (int i = 0; i < cavalo_vertical; i++) {
 		printf("Baixo\n");
 	}
+	
 	int j = 0;
+	
 	while (j < cavalo_horizontal) {
 		printf("Esquerda\n");
 		j++;
