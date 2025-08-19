611110e808cfc5c4da9fc827f713657bd6ea37ca
Gleidson <152668886+hariuky@users.noreply.github.com>
2025-08-19 17:54:10 -0300

    xadrezNovato.c

--- xadrez.c
+++ xadrez.c
@@ -1,32 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-// Desafio de Xadrez - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
-// O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
-
-int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
-
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
-
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
-
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
-
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
-
-    // Nível Mestre - Funções Recursivas e Loops Aninhados
-    // Sugestão: Substitua as movimentações das peças por funções recursivas.
-    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
-
-    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
-    // Inclua o uso de continue e break dentro dos loops.
-
-    return 0;
+int main(int argc, char *argv[]) {	
+	int var = 0;
+	const torre = 5;
+	const bispo = 5;
+	const rainha = 8;
+				
+	printf("Torre movendo...\n");
+	for(int i = 0; i < torre; i++){
+		printf("Direita\n");
+	}
+		
+	printf("\nBispo movendo...\n");
+	while(var < bispo){
+		printf("Cima-direita\n");
+		var++;
+	}
+				
+	printf("\nRainha movendo...\n");
+	var = 0; 
+	do{
+		printf("Esquerda\n");
+		var ++;
+	}while(var < rainha);
+		
+					
+	return 0;
 }
